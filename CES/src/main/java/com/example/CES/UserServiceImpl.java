a8a9120717a83a9480ccc9cc984fd1b29395b6c6
Hunter H <52612214+Elder-ittehgapS@users.noreply.github.com>
2023-03-22 14:18:18 -0400

    changed role name

--- CES/src/main/java/com/example/CES/UserServiceImpl.java
+++ CES/src/main/java/com/example/CES/UserServiceImpl.java
@@ -31,7 +31,7 @@ public class UserServiceImpl implements UserService {
         // encrypt the password using spring security
         user.setPassword(passwordEncoder.encode(userDto.getPassword()));
 
-        Role role = roleRepository.findByName("Customer");
+        Role role = roleRepository.findByName("ROLE_USER");
         if(role == null){
             role = checkRoleExist();
         }
@@ -62,7 +62,7 @@ public class UserServiceImpl implements UserService {
 
     private Role checkRoleExist(){
         Role role = new Role();
-        role.setName("Customer");
+        role.setName("RULE_USER");
         return roleRepository.save(role);
     }
 }
